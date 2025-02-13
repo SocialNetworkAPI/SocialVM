2af66e3b241b0e9ceab917f6ac35c3cca35fa9d1
Source Graph A.I. <117184625+UniInc@users.noreply.github.com>
2025-02-12 22:18:06 -0600

    Update create.sh

--- hyper/Distrobox/create.sh
+++ hyper/Distrobox/create.sh
@@ -1,2 +1,4 @@
+# https://distrobox.it/useful_tips/#create-a-distrobox-with-a-custom-home-directory
+
 . name.sh
-distrobox create --name $name --volume ~/.uni/hyper/distrobox/$name
+distrobox create --name $name --home ~/.uni/hyper/distrobox/$name
